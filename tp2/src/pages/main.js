3259bca4a55b6fff69e83b260d609e42e522b6f4
Unknown <breno_lopes_10@yahoo.com.br>
2019-07-19 00:53:40 -0300

    ....

--- tp2/src/pages/main.js
+++ tp2/src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import api from '../services/api';
-// import Axios from 'axios';
+
 
 
 export default class Main extends Component {
